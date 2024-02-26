d7a5d13385c07554157c652fb11e503e56c27ca9
AdityaManojShinde <aditya.manoj.shinde@gmail.com>
2024-02-26 16:24:17 +0530

    added splash screen

--- lib/models/routes.dart
+++ lib/models/routes.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:newsapp/views/screens/home_screen.dart';
+import 'package:newsapp/views/screens/splash_screen.dart';
 
 final GoRouter router = GoRouter(
   routes: <RouteBase>[
     GoRoute(
       path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SplashScreen();
+      },
+    ),
+    GoRoute(
+      path: '/home',
       builder: (BuildContext context, GoRouterState state) {
         return const Home();
       },
---
+++ lib/views/screens/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset(
+                'assets/images/world.jpg',
+                fit: BoxFit.cover,
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 1.7,
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  '"Beyond the headlines, discover the stories shaping our world."',
+                  softWrap: true,
+                  style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ),
+            ),
+            Center(
+              child: ElevatedButton(
+                  onPressed: () {
+                    context.go('/home');
+                  },
+                  child: const Text('Get Started')),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
